b2ac18192d8e95699258c342e65a49b300677ac2
BaranEsmer <116164379+Baran1903@users.noreply.github.com>
2024-03-20 15:02:10 +0300

    Add files via upload

---
+++ Kütüphane_uyg/Kütüphane_uyg/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace Kütüphane_uyg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.Jet.OLeDB.4.0;Data Source=Kütüphane.mdb;");
+        OleDbCommand komut;
+        OleDbDataAdapter da;
+        
+
+
+        void göster()
+        {
+            baglanti.Open();
+
+            da = new OleDbDataAdapter("Select * From Öğrenciler", baglanti);
+            DataTable arabalar = new DataTable();
+
+            da.Fill(arabalar);
+            dataGridView1.DataSource = arabalar;
+            baglanti.Close();
+    
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sorgu = "INSERT INTO Öğrenciler (AdıSoyadı , Sınıfı , OkulNo ) VALUES ('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "')";
+            OleDbCommand komut = new OleDbCommand(sorgu, baglanti);
+
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            göster();
+
+            
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            göster();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sorgu = "UPDATE Öğrenciler SET Sınıfı = '" + textBox2.Text + "', OkulNo = '" + textBox3.Text + "' WHERE AdıSoyadı = '" + textBox1.Text + "'";
+
+            OleDbCommand komut = new OleDbCommand(sorgu, baglanti);
+
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            göster();
+        }
+    }
+}
---
+++ Kütüphane_uyg/Kütüphane_uyg/Form2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace Kütüphane_uyg
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection bağlanti = new OleDbConnection("Provider=Microsoft.Jet.OLeDB.4.0;Data Source=Kütüphane.mdb;");
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM kullanici";
+            OleDbCommand komut = new OleDbCommand(sql, bağlanti);
+            bağlanti.Open();
+            OleDbDataReader oku = komut.ExecuteReader();
+            oku.Read();
+            if (oku.GetValue(0).ToString() == textBox1.Text && oku.GetValue(1).ToString() == textBox2.Text)
+            {
+                Form1 form1 = new Form1();
+                this.Hide();
+                form1.Show();
+                
+            }
+            else
+            {
+                MessageBox.Show("kullanıcı adı veya parola yanlış");
+            }
+            bağlanti.Close();
+        }
+    }
+}
