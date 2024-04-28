4516b224dda80561cbf6da2e1cc3384588b58e4a
BaranEsmer <116164379+Baran1903@users.noreply.github.com>
2024-04-28 19:20:22 +0300

    Add files via upload

---
+++ NTP proje/uyg-1/kitapislemleri.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace uyg_1
+{
+    public partial class kitapislemleri : Form
+    {
+        public kitapislemleri()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection baglan = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:Kütüphane.mdb");
+        OleDbCommand komut;
+        OleDbDataAdapter da;
+
+        void goster()
+        {
+            baglan.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter("SELECT * FROM Kitaplar", baglan);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+
+            dataGridView1.DataSource = tablo;
+            baglan.Close();
+        
+        
+        
+        
+        }
+
+        private void kitapislemleri_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sorgu = "INSERT INTO Kitaplar (Kitap Adı,Yazarı,Sayfası,Teması,Türü) VALUES ('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','"+textBox4+"','"+textBox5+"')";
+
+            OleDbCommand komut = new OleDbCommand(sorgu, baglan);
+
+            baglan.Open();
+            komut.ExecuteNonQuery();
+            baglan.Close();
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sorgu = "DELETE FROM Kitaplar WHERE Kitap Adı = '" + textBox1.Text + "'";
+
+            OleDbCommand komut = new OleDbCommand(sorgu, baglan);
+
+            baglan.Open();
+            komut.ExecuteNonQuery();
+            baglan.Close();
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sorgu = "UPDATE Kitaplar SET Yazarı = '" + textBox2.Text + "', Sayfası = '" + textBox3.Text + "', Teması = '" + textBox4.Text + "' , Türü = '" + textBox5.Text + "' WHERE Kitap Adı = '" + textBox1.Text + "'";
+
+            OleDbCommand komut = new OleDbCommand(sorgu, baglan);
+
+            baglan.Open();
+            komut.ExecuteNonQuery();
+            baglan.Close();
+
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            textBox5.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            
+        }
+    }
+}
---
+++ NTP proje/uyg-1/sifredegistirme.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace uyg_1
+{
+    public partial class sifredegistirme : Form
+    {
+        public sifredegistirme()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection baglan = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:Kütüphane.mdb");
+        OleDbCommand komut;
+        OleDbDataAdapter da;
+
+        void olustur()
+        {
+            baglan.Open();
+            da = new OleDbDataAdapter("Select * From Kütüphane", baglan);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            baglan.Close();
+        
+     
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        int yenisifre;
+        int tekrarsifre;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            yenisifre = Convert.ToInt32(textBox2.Text);
+            tekrarsifre = Convert.ToInt32(textBox3.Text);
+
+
+           string sgln = ("Select * From klnc ");
+           komut = new OleDbCommand(sgln, baglan);
+           baglan.Open();
+           OleDbDataReader okuyucu = komut.ExecuteReader();
+
+           while (okuyucu.Read())
+           {
+               if (okuyucu.GetValue(1).ToString() == textBox1.Text && okuyucu.GetValue(2).ToString() == textBox2.Text)
+               {
+                   if (yenisifre == tekrarsifre)
+                   {
+                       string om = "UPTADE klnc set Sifre = '" + yenisifre + "' WHERE KullaniciAdi = '" + textBox1.Text + "'";
+                       komut = new OleDbCommand(om, baglan);
+                       komut.ExecuteNonQuery();
+
+                   }
+
+               }               
+
+           }
+           baglan.Close();
+           Form1 arda = new Form1();
+           arda.Show();
+           this.Hide();
+
+        
+            
+        }
+
+        private void sifredegistirme_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
